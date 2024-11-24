b81c8e46b402433100258389f67e189b7aede025
AizaDiusheeva <165205849+AizaDiusheeva@users.noreply.github.com>
2024-11-24 19:51:30 +0100

    Add files via upload

---
+++ homework16/GPT-3-main-Aiza/GPT-3-main/js/script.js
@@ -0,0 +1,9 @@
+const menu = document.querySelector(".menu");
+const hamburger = document.querySelector(".hamburger-menu");
+hamburger.onclick = () => {
+    menu.classList.toggle("show");
+}
+const form = document.querySelector(".form");
+const foto = document.querySelector(".foto");
+const info = document.querySelector(".into");
+const block1 = document.querySelector(".text");
